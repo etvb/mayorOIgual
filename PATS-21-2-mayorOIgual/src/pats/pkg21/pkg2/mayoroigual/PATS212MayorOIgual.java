59c3900f0bb27c01c099fd5e8c40a8a9b1931d47
Edgardo Torres Vanden Bouch <etvb66@hotmail.com>
2017-09-04 06:32:48 -0500

    archivos base

---
+++ PATS-21-2-mayorOIgual/src/pats/pkg21/pkg2/mayoroigual/PATS212MayorOIgual.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pats.pkg21.pkg2.mayoroigual;
+
+/**
+ *
+ * @author edgardo
+ */
+public class PATS212MayorOIgual {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
