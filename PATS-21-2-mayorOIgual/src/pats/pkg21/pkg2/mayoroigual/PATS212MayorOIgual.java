a84875f33061e1085160a22abb24ec8da2bd33b7
Edgardo Torres Vanden Bouch <etvb66@hotmail.com>
2017-09-04 06:44:00 -0500

    agregamos las variables, realizamos las operaciones

--- PATS-21-2-mayorOIgual/src/pats/pkg21/pkg2/mayoroigual/PATS212MayorOIgual.java
+++ PATS-21-2-mayorOIgual/src/pats/pkg21/pkg2/mayoroigual/PATS212MayorOIgual.java
@@ -5,6 +5,8 @@
  */
 package pats.pkg21.pkg2.mayoroigual;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author edgardo
@@ -15,7 +17,21 @@ public class PATS212MayorOIgual {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        int num1, num2;
+        
+        num1 = Integer.parseInt(JOptionPane.showInputDialog("Ingresa el primer valor"));
+        num2 = Integer.parseInt(JOptionPane.showInputDialog("Ingresa el segundo valor"));
+        
+        if(num1 < num2){
+            JOptionPane.showMessageDialog(null, num2 + " Es mayor que " + num1 );
+        }else if(num1 > num2){
+            JOptionPane.showMessageDialog(null, num1 + " es mayor que " + num2);
+        }else{
+            JOptionPane.showMessageDialog(null, "Son iguales");
+        }
+        
+        
     }
     
 }
